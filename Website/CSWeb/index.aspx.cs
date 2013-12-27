ac17255e0b01699211104b0476b4257d8473873a
CONVERSION\gawakian <CONVERSION\gawakian>
2013-12-27 20:38:16 +0000

    point everything to D2

--- Website/CSWeb/index.aspx.cs
+++ Website/CSWeb/index.aspx.cs
@@ -15,13 +15,13 @@ namespace CSWeb
 
             if (Request.QueryString != null && Request.QueryString.ToString().Length > 1)
             {
-                 Response.Redirect("/B2?" + Request.QueryString + "&referrer=" + Request.Url);
+                 Response.Redirect("/D2?" + Request.QueryString + "&referrer=" + Request.Url);
             }
 
             else
             
             {
-                 Response.Redirect("/B2?referrer=" + Request.Url);
+                 Response.Redirect("/D2?referrer=" + Request.Url);
             }
 
            
